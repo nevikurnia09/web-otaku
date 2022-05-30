bf202340ae43aa10884dc35d89f6454cb6cb2e96
nevikurnia09 <106400634+nevikurnia09@users.noreply.github.com>
2022-05-30 22:59:27 +0700

    Update VideoPlayer.js

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 const VideoPlayer = ({url}) => {
     return (
-
-<iframe id="istream" src="{url}" class="responsive-stream" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
-    
-    )
+<link rel="stylesheet" href="//cdn.flowplayer.com/releases/native/3/stable/style/flowplayer.css">
+<script src="//cdn.flowplayer.com/releases/native/3/stable/flowplayer.min.js"></script>
+<!-- Optional plugins -->
+<script src="//cdn.flowplayer.com/releases/native/3/stable/plugins/hls.min.js"></script>
+<div
+  class="flowplayer-embed-container"
+  style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width:100%;">
+  <iframe
+    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+    webkitAllowFullScreen mozallowfullscreen allowfullscreen
+    src="{url}"
+    title="0" byline="0" portrait="0"
+    width="640" height="360"
+    frameborder="0"
+    allow="autoplay"/ >
+</div>
+)
 }
 
 export default VideoPlayer
