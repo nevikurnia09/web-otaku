4e01a5e30862f76d4783fbe781cf9ffba9cb4f2b
nevikurnia09 <106400634+nevikurnia09@users.noreply.github.com>
2022-05-30 22:52:43 +0700

    Update VideoPlayer.js

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -3,20 +3,7 @@ import React from 'react'
 const VideoPlayer = ({url}) => {
     return (
 
-        <iframe sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation"
-            className='mb-4' title="outer" src={url} style = {{
-            position: "relative",
-            boxShadow: "2% 2% 4% rgba(0,0,0,0.5)",
-            width: "100%",
-            height: "700px"
-        }} 
-        frameBorder="0"
-        allowFullScreen={true}
-        marginHeight="0"
-        marginWidth="0"
-        scrolling="no" 
-
-        />
+<iframe id="istream" src="{url}" class="responsive-stream" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
     
     )
 }
